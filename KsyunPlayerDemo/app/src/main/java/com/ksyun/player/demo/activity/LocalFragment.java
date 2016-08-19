481fba2bde3371f5d691a607bdfb1f820872a0c9
liubohua <LIUBOHUA@KINGSOFT.COM>
2016-08-19 13:20:26 +0800

    load Local

--- KsyunPlayerDemo/app/src/main/java/com/ksyun/player/demo/activity/LocalFragment.java
+++ KsyunPlayerDemo/app/src/main/java/com/ksyun/player/demo/activity/LocalFragment.java
@@ -3,12 +3,15 @@ package com.ksyun.player.demo.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ksyun.player.demo.R;
 import com.ksyun.player.demo.util.Video;
@@ -17,19 +20,20 @@ import com.ksyun.player.demo.model.GetList;
 import java.util.ArrayList;
 
 
-public class LocalFragment extends android.app.Fragment {
+public class LocalFragment extends android.app.Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
+    private static final int UPDATE = 1;
     private ListView listView;
     private JieVideoListViewAdapter madapter;
-    private ArrayList<Video> listVideos;
-
-
-
+    private ArrayList<Video> showlistVideos;
+    private SwipeRefreshLayout swipeLayout;
+    public static Handler mHandler;
+    private GetList getList;
+    private boolean updateok = false;
 
     public LocalFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,26 +45,67 @@ public class LocalFragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        listVideos = new ArrayList<Video>();
-        GetList getList = new GetList();
-        getList.getVideoFile(listVideos, Environment.getExternalStorageDirectory());
+        showlistVideos = new ArrayList<Video>();
+        getList = new GetList();
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch(msg.what){
+                    case UPDATE:
+                        if(updateok){
+                            updatelist();
+                            swipeLayout.setRefreshing(false);
+                            Toast.makeText(getActivity(), "更新成功", Toast.LENGTH_SHORT).show();
+                        }else{
+                            swipeLayout.setRefreshing(false);
+                            Toast.makeText(getActivity(), "更新失败,请等待加载完毕", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 2:
+                        if(msg.obj instanceof ArrayList) {
+                            showlistVideos.clear();
+                            showlistVideos.addAll((ArrayList<Video>)msg.obj);
+                            updatelist();
+                        }
+                        break;
+                    case 3:
+                        if(msg.obj instanceof ArrayList) {
+                            updateok = true;
+                            showlistVideos.clear();
+                            showlistVideos.addAll((ArrayList<Video>)msg.obj);
+                        }
+                }
+            }
+        };
         View view = inflater.inflate(R.layout.fragment_local, container, false);
+
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
         listView = (ListView)view.findViewById(R.id.list_local_frag);
-        madapter = new JieVideoListViewAdapter(getActivity(),listVideos);
-        listView.setAdapter(madapter);
+        swipeLayout.setOnRefreshListener(this);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Video v = listVideos.get(position);
-                Log.e("adasdasd",v.getPath());
+                Video v = showlistVideos.get(position);
                 Intent intent = new Intent(getActivity(),VideoPlayerActivity.class);
                 intent.putExtra("path", v.getPath());
                 startActivity(intent);
             }
         });
+
+        getList.startThread(showlistVideos, Environment.getExternalStorageDirectory());
         return view;
     }
+    public void updatelist(){
+        madapter = new JieVideoListViewAdapter(getActivity(),showlistVideos);
+        listView.setAdapter(madapter);
+    }
 
-
+    @Override
+    public void onRefresh() {
+        Message msg = new Message();
+        msg.what = UPDATE;
+        mHandler.sendMessageDelayed(msg,3000);
+    }
 }
 
